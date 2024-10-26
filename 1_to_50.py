42e76076e771dec6d2c37a85e4f27bca5c8d42df
Gabriel <you@example.com>
2024-10-27 01:28:08 +0800

    I added if the input is less than 1 or greater than 50 break to stop the loop

--- 1_to_50.py
+++ 1_to_50.py
@@ -1,3 +1,9 @@
 
 while True:
+        
     user_input = int(input("Please input a number between 1 and 50 (to exit input any other numbers):"))
+    
+    if user_input < 1 or user_input > 50:
+        
+        break
+    
