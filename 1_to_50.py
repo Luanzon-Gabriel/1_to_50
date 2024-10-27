5e66f8ff2540e6811c6abe111f0f7082dc00626b
Gabriel <you@example.com>
2024-10-27 11:20:21 +0800

    I added error handling try except so that the code will not error

--- 1_to_50.py
+++ 1_to_50.py
@@ -6,27 +6,29 @@ range_31_40 = 0
 range_41_50 = 0
 
 while True:
-    
-    user_input = int(input("Please input a number between 1 and 50 (to exit input any other numbers):"))
-    
-    if user_input < 1 or user_input > 50:
-        break
+        try:
+                user_input = int(input("Please input a number between 1 and 50 (to exit input any other numbers):"))
+                
+                if user_input < 1 or user_input > 50:
+                        break
+            
+                if 1 <= user_input <= 10:
+                        range_1_10 += 1
+
+                elif 11 <= user_input <= 20:
+                        range_11_20 += 1
+
+                elif 21 <= user_input <= 30:
+                        range_21_30 += 1
+
+                elif 31 <= user_input <= 40:
+                        range_31_40 += 1
+
+                elif 41 <= user_input <= 50:
+                        range_41_50 += 1
+        except ValueError:
+                print("Please input numbers only.")                
         
-    if 1 <= user_input <= 10:
-            range_1_10 += 1
-
-    elif 11 <= user_input <= 20:
-            range_11_20 += 1
-
-    elif 21 <= user_input <= 30:
-            range_21_30 += 1
-
-    elif 31 <= user_input <= 40:
-            range_31_40 += 1
-
-    elif 41 <= user_input <= 50:
-            range_41_50 += 1
-   
 print("Number of inputs in each range:")
 print(f"1 - 10  = {range_1_10}")
 print(f"11 - 20 = {range_11_20}")
